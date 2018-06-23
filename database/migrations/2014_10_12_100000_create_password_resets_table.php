b3734643a432ef6195dca2bcd0f09182804c9dce
Promsurin Phutthammawong <promsurinm@hotmail.com>
2018-06-23 23:09:28 +0700

    Add carbon

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -14,8 +14,8 @@ class CreatePasswordResetsTable extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->text('email',50)->index();
-            $table->text('token');
+            $table->string('email',50)->index();
+            $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
     }

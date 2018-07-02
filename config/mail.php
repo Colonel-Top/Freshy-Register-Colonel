b8cce67e5ba474edabbafcc91f13221d77b5cccd
Promsurin Phutthammawong <promsurinm@hotmail.com>
2018-07-03 00:44:44 +0700

    Fix IOS BUg

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.gmail.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('colonel.techcorp@gmail.com', 'hello@example.com'),
+        'name' => env('Freshy TU 61 System', 'Example'),
     ],
 
     /*
